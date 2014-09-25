843ec8ef2ce03a62b4d12a1862f57cfd8280f007
Chris Lamb <lamb.chrisr@gmail.com>
2014-09-25 09:38:32 -0400

    Add method to allow reseting the mock redis instance when testing.

--- lib/sidekiq-unique-jobs/testing.rb
+++ lib/sidekiq-unique-jobs/testing.rb
@@ -3,4 +3,8 @@ module SidekiqUniqueJobs
   def self.redis_mock
      @redis_mock ||= MockRedis.new
   end
+
+  def self.reset_redis_mock
+    @redis_mock = nil
+  end
 end
--- spec/lib/sidekiq_testing_enabled_spec.rb
+++ spec/lib/sidekiq_testing_enabled_spec.rb
@@ -1,34 +1,39 @@
 require 'spec_helper'
 require 'sidekiq/worker'
 require "sidekiq-unique-jobs"
+require 'sidekiq-unique-jobs/testing'
 require 'sidekiq/scheduled'
 require 'sidekiq-unique-jobs/middleware/server/unique_jobs'
 require 'rspec-sidekiq'
 
 describe "When Sidekiq::Testing is enabled" do
+  before do
+    SidekiqUniqueJobs.reset_redis_mock
+  end
+
   describe 'when set to :fake!', sidekiq: :fake do
     context "with unique worker" do
+      let(:param) { 'work' }
+      before do
+        UniqueWorker.perform_async(param)
+        UniqueWorker.perform_async(param)
+      end
+
       it "does not push duplicate messages" do
-        param = 'work'
-        expect(UniqueWorker.jobs.size).to eq(0)
-        UniqueWorker.perform_async(param)
-        expect(UniqueWorker.jobs.size).to eq(1)
-        expect(UniqueWorker).to have_enqueued_job(param)
-        UniqueWorker.perform_async(param)
         expect(UniqueWorker.jobs.size).to eq(1)
       end
 
+      it "enques the job" do
+        expect(UniqueWorker).to have_enqueued_job(param)
+      end
+
       it "adds the unique_hash to the message" do
-        param = 'hash'
         hash = SidekiqUniqueJobs::PayloadHelper.get_payload(UniqueWorker, :working, [param])
-        UniqueWorker.perform_async(param)
-        expect(UniqueWorker.jobs.size).to eq(1)
         expect(UniqueWorker.jobs.first['unique_hash']).to eq(hash)
       end
     end
 
     context "with non-unique worker" do
-
       it "pushes duplicates messages" do
         param = 'work'
         expect(MyWorker.jobs.size).to eq(0)
